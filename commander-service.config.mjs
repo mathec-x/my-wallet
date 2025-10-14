f10442517781caf98b297ea55a3c5e3201b46164
matheus correa <mathec@live.com>
2025-10-14 10:21:03 -0300

    feat: implements comands cli generator project

---
+++ commander-service.config.mjs
@@ -0,0 +1,56 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { dirname } from 'path';
+import { styleText } from 'util';
+
+export class CommanderService {
+  paths = {
+    privatePage: 'src/app/(private)',
+    publicPage: 'src/app/(public)',
+    drawer: 'src/app/@drawer',
+    action: 'src/app/actions',
+    component: 'src/app/components',
+    hook: 'src/app/hooks',
+    provider: 'src/app/providers',
+    useCase: 'src/server/application/useCases',
+    domainService: 'src/server/domain/services',
+  };
+
+  sanitizePath(name) {
+    if (name.startsWith('/')) name = name.slice(1);
+    if (name.endsWith('/')) name.slice(0, -1);
+    return name;
+  };
+
+  camelCase(name) {
+    return name
+      .split(/[^a-zA-Z0-9]/)
+      .map((word, index) => {
+        if (index === 0) return word.toLowerCase();
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      })
+      .join('');
+
+  }
+  pascalCase(name) {
+    return name
+      .split(/[^a-zA-Z0-9]/)
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      })
+      .join('');
+  }
+
+  createFile(fileName, content) {
+    const dirName = dirname(fileName);
+    if (!existsSync(dirName)) {
+      console.log(styleText('cyan', `Creating directory ${dirName}`));
+      mkdirSync(dirName, { recursive: true });
+    }
+    if (existsSync(fileName)) {
+      console.log(styleText('redBright', `File already exists ${fileName}`));
+      return;
+    }
+    console.log(styleText('cyan', `Creating file ${fileName}`));
+    writeFileSync(fileName, content);
+  }
+}
---
+++ commander.config.mjs
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { CommanderService } from './commander-service.config.mjs';
+
+const program = new Command();
+const commands = new CommanderService();
+
+program
+  .command('page')
+  .description('Add a new page to the project')
+  .argument('<name>', 'Name of the page to add')
+  .option('-p, --private', 'Indicates if the page is private')
+  .action(function (name, args) {
+    const path = args.private ? commands.paths.privatePage : commands.paths.publicPage;
+    const fnName = commands.pascalCase(name);
+    commands.createFile(
+      `${path}/${commands.sanitizePath(name)}/page.tsx`,
+      `export default async function ${fnName}Page() {\r\treturn (\r\t\t<div>Page ${fnName}</div>\r\t);\r}`
+    );
+  });
+
+program
+  .command('drawer')
+  .description('Add a new drawer to the project')
+  .argument('<name>', 'Name of the drawer to add')
+  .action(function (name) {
+    const path = commands.paths.drawer;
+    const fnName = commands.pascalCase(name);
+    commands.createFile(
+      `${path}/(.)${commands.sanitizePath(name)}/page.tsx`,
+      `export default async function ${fnName}Drawer() {\r\treturn (\r\t\t<div>Drawer ${fnName}</div>\r\t);\r}`
+    );
+  });
+
+program
+  .command('action')
+  .description('Add a new action to the project')
+  .argument('<name>', 'Name of the action to add')
+  .action(function (name) {
+    const path = commands.paths.actions;
+    const fnName = commands.pascalCase(name);
+    commands.createFile(
+      `${path}/(.)${commands.sanitizePath(name)}/page.tsx`,
+      `export default function ${fnName}Drawer() {\r\treturn (\r\t\t<div>Drawer ${fnName}</div>\r\t);\r}`
+    );
+  });
+
+program
+  .command('component')
+  .description('Add a new component to the project')
+  .argument('<name>', 'Name of the component to add')
+  // Componentes compostos (formulários, listas, etc)
+  .option('-c, --composite', 'Complete session component, dashboard, composite forms, etc')
+  // Componentes de interface (cards, modals, tables, etc)
+  .option('-u --ui', 'UI component, cards, modals, tables, etc')
+  // Componentes de layout (header, footer, sidebar, etc)
+  .option('-l --layout', 'Layout component, header, footer, sidebar, etc')
+  // Componentes primitivos (avatar, badge, button, input, etc)
+  .option('-p --primitive', 'Primitive component, avatar, badge, button, etc')
+  .action(function (name, args) {
+    const folder = args.composite ? '/composites' : args.ui ? '/ui' : args.layout ? '/layouts' : args.primitive ? '/primitives' : '/elements';
+    const type = args.composite ? 'composite' : args.ui ? 'ui' : args.layout ? 'layout' : args.primitive ? 'primitive' : 'element';
+    const path = commands.paths.component + folder;
+    const fnName = commands.pascalCase(name);
+    commands.createFile(`${path}/${fnName}/${name}.module.css`, '');
+    commands.createFile(
+      `${path}/${fnName}/${name}.${type}.tsx`,
+      `export default function ${fnName}() {\r\treturn (\r\t\t<div>${fnName}</div>\r\t);\r}`
+    );
+  });
+
+program
+  .command('useCase')
+  .description('Add a new use case in server')
+  .argument('<name>', 'Name of the use case to add')
+  .action(function (name) {
+    const path = commands.paths.useCase;
+    const fnName = commands.pascalCase(name);
+    commands.createFile(`${path}/${name}/${name}.spec.ts`, '');
+    commands.createFile(
+      `${path}/${name}/${name}.useCase.ts`,
+      `export class ${fnName} {\r\tconstructor() { };\r}`
+    );
+  });
+
+program
+  .command('domain')
+  .description('Add a new domain service in server')
+  .argument('<name>', 'Name of the domain service to add')
+  .action(function (name) {
+    const path = commands.paths.domainService;
+    const fnName = commands.pascalCase(name);
+    commands.createFile(`${path}/${name}/${name}.spec.ts`, '');
+    commands.createFile(
+      `${path}/${name}/${name}.service.ts`,
+      `export class ${fnName} {\r\tconstructor() { };\r}`
+    );
+  });
+
+
+program.parse();
