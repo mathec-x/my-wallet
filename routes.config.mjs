e0bb0073d7e75de0742dc135efb7b17d4b2a8e2d
matheus correa <mathec@live.com>
2025-10-14 07:58:40 -0300

    feat: add private routes setup

---
+++ routes.config.mjs
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { existsSync, mkdirSync, readdirSync, writeFileSync } from 'fs';
+console.clear();
+const privateRoutes = readdirSync('src/app/(private)', {
+  recursive: true,
+  withFileTypes: true
+})
+  .filter((file) => file.isFile())
+  .map((file) => file.parentPath.replace('src/app/(private)', ''));
+
+if (!existsSync('./src/server/infra/routes')) {
+  console.log('Creating directory: ./src/server/infra/routes');
+  mkdirSync('./src/server/infra/routes', { recursive: true });
+}
+
+writeFileSync(
+  './src/server/infra/routes/privateRoutes.ts',
+  `export const privateRoutes = ${JSON.stringify(privateRoutes, null, 2).replace(/"/g, "'")} as string[];`
+);
+console.log('Generate private routes setup completed:', privateRoutes);
---
+++ src/server/infra/routes/privateRoutes.ts
@@ -0,0 +1,5 @@
+export const privateRoutes = [
+  '/dashboard',
+  '/menu',
+  '/dashboard/[uuid]'
+] as string[];
