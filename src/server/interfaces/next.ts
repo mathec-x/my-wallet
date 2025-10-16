c55c87f2ce173b8ce9f67d12e62c59f4b7406bc9
matheus correa <mathec@live.com>
2025-10-16 01:41:14 -0300

    chore: new response type

--- src/server/interfaces/next.ts
+++ src/server/interfaces/next.ts
@@ -4,3 +4,6 @@ export interface NextAsyncPageProps<P = unknown> {
   params: Promise<P>;
   searchParams: Promise<ReadonlyURLSearchParams>;
 }
+
+type ResponseServiceData<T> = T extends { data: infer U } ? U : never;
+export type ResponseServiceAsync<T extends (...args: never) => unknown> = ResponseServiceData<Awaited<ReturnType<T>>>;
