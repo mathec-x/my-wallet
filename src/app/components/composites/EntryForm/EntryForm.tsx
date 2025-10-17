2f66afc2f6ff12ea1bf627cef0bf06bd8a644e0a
matheus correa <mathec@live.com>
2025-10-17 15:58:55 -0300

    feat: improve with react hooks

--- src/app/components/composites/EntryForm/EntryForm.tsx
+++ src/app/components/composites/EntryForm/EntryForm.tsx
@@ -1,25 +1,41 @@
 'use client';
 
-import { Entry } from '@/app/actions/entries/entries.actions';
+import { entriesUpdateAction, Entry } from '@/app/actions/entries/entries.actions';
 import FullScreenModal from '@/app/components/elements/Modal';
 import FormControlSchema from '@/app/components/primitives/Form/FormControlSchema';
+import useModalHandler from '@/app/hooks/useModalHandler';
 import { EntryUpdateFormSchema, entryUpdateFormSchema } from '@/shared/schemas/entryUpdateForm';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import { useState, useTransition } from 'react';
 import FlexBox from '../../elements/FlexBox';
 
 interface EntryFormProps {
+  editorModalName: string;
   entry: Entry | undefined;
+  onUpdate: (data: EntryUpdateFormSchema) => ReturnType<typeof entriesUpdateAction>;
 }
 
-const EntryForm = ({ entry }: EntryFormProps) => {
-  const handleUpdate = async (data: EntryUpdateFormSchema) => {
-    console.log('handleUpdate', data);
+const EntryForm = ({ entry, editorModalName, onUpdate }: EntryFormProps) => {
+  const modal = useModalHandler(editorModalName);
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const handleUpdate = (data: EntryUpdateFormSchema) => {
+    startTransition(async () => {
+      const res = await onUpdate(data);
+      if (res.success) {
+        setError(null);
+        modal.close();
+      } else {
+        setError(res.message || 'Erro ao atualizar a entrada. Tente novamente mais tarde.');
+      }
+    });
   };
 
   return (
     <FullScreenModal
-      name="entry"
+      name={modal.name}
       title={<>Editar <b>{entry?.title}</b></>}
       description={`Aqui você pode atualizar as informações da sua ${entry?.type === 'INCOME' ? 'entrada' : 'saída'}.`}>
       {entry ?
@@ -28,9 +44,10 @@ const EntryForm = ({ entry }: EntryFormProps) => {
           value={entry}
           onSubmit={handleUpdate}
           schema={entryUpdateFormSchema}
+          errorMessage={error}
         >
           <Divider variant='fullWidth' sx={{ mt: 8 }} />
-          <Button type='submit' form='form-entry-update' variant='contained' fullWidth>
+          <Button loading={isPending} type='submit' form='form-entry-update' variant='contained' fullWidth>
             Atualizar
           </Button>
         </FormControlSchema>
