b8b69aa945b595a15a5a8d6f29d4cb63c75b7e09
matheus correa <mathec@live.com>
2025-10-17 15:58:38 -0300

    feat: use client page

--- src/app/(private)/dashboard/[uuid]/page.tsx
+++ src/app/(private)/dashboard/[uuid]/page.tsx
@@ -1,12 +1,13 @@
 import { entriesListAction } from '@/app/actions/entries/entries.actions';
-import EntryForm from '@/app/components/composites/EntryForm/EntryForm';
-import EntryList from '@/app/components/layouts/EntryList/EntryList.layout';
+import Dashboard from '@/app/components/layouts/Dashboard/Dashboard.layout';
 import { NextAsyncPageProps } from '@/server/interfaces/next';
-import Grid from '@mui/material/Grid';
 
+
+const entrySearchParam = 'entry';
 export default async function DashboardPage(props: NextAsyncPageProps<{ uuid: string }, { entry?: string }>) {
   const { uuid: accountUuid } = await props.params;
-  const { entry: entryUuid } = await props.searchParams;
+  const searchParamsProps = await props.searchParams;
+  const entryUuid = searchParamsProps[entrySearchParam];
 
   const entries = await entriesListAction({ accountUuid });
   if (!entries.success) {
@@ -14,22 +15,11 @@ export default async function DashboardPage(props: NextAsyncPageProps<{ uuid: st
   }
 
   return (
-    <Grid container spacing={2} sx={{ mt: 1 }} height='calc(100vh - 100px)'>
-      <Grid size={{ xs: 12, sm: 6 }}>
-        <EntryList
-          accountUuid={accountUuid}
-          entries={entries.data.filter(entry => entry.type === 'INCOME')}
-          type='INCOME'
-        />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6 }}>
-        <EntryList
-          accountUuid={accountUuid}
-          entries={entries.data.filter(entry => entry.type === 'EXPENSE')}
-          type='EXPENSE'
-        />
-      </Grid>
-      <EntryForm entry={entries.data.find(entry => entry.uuid === entryUuid)} />
-    </Grid>
+    <Dashboard
+      accountUuid={accountUuid}
+      entries={entries.data}
+      entryUuid={entryUuid}
+      entrySearchParam={entrySearchParam}
+    />
   );
 }
