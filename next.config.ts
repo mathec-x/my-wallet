07b222bb8322b99896304d3c6a396d9e2421d6d6
matheus correa <mathec@live.com>
2025-10-18 19:07:51 -0300

    fix: lint build setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    dirs: [
+      'src/app',
+      'src/shared',
+      'src/server/application',
+      'src/server/domain',
+    ],
+  },
 };
 
 export default nextConfig;
