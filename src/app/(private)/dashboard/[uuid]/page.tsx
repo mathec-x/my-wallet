ad92751559268216150991fdcbd35e62f404573d
matheus correa <mathec@live.com>
2025-11-15 09:16:50 -0300

    fix: ui add meta title

--- src/app/(private)/dashboard/[uuid]/page.tsx
+++ src/app/(private)/dashboard/[uuid]/page.tsx
@@ -4,6 +4,7 @@ import EntryBalance from '@/app/components/ui/EntryBalance/EntryBalance.ui';
 import { EntriesProvider } from '@/app/providers/entries/EntriesProvider';
 import MoneyIcon from '@mui/icons-material/AttachMoney';
 import Grid from '@mui/material/Grid';
+import Head from 'next/head';
 
 export default async function DashboardPageLayout(props: PageProps<'/dashboard/[uuid]'>) {
   const { uuid: accountUuid } = await props.params;
@@ -21,22 +22,27 @@ export default async function DashboardPageLayout(props: PageProps<'/dashboard/[
   }
 
   return (
-    <EntriesProvider entries={account.data.entries} accountUuid={account.data.uuid}>
-      <Grid container spacing={2} alignContent='flex-start' sx={{ mt: 1 }} height='calc(100vh - 100px)'>
-        <Grid size={{ xs: 12 }}>
-          <EntryBalance accountUuid={accountUuid} />
+    <>
+      <Head>
+        <title>Dashboard - {account.data.name}</title>
+      </Head>
+      <EntriesProvider entries={account.data.entries} accountUuid={account.data.uuid}>
+        <Grid container spacing={2} alignContent='flex-start' sx={{ mt: 1 }} height='calc(100vh - 100px)'>
+          <Grid size={{ xs: 12 }}>
+            <EntryBalance accountUuid={accountUuid} />
+          </Grid>
+          <GridDashboardLayout
+            entryUuid={entryUuid?.toString()}
+            listItemCollapseProps={{
+              disablePadding: true,
+              defaultOpen: true,
+              divider: false,
+              icon: <MoneyIcon />,
+              avatarVariant: 'circular'
+            }}
+          />
         </Grid>
-        <GridDashboardLayout
-          entryUuid={entryUuid?.toString()}
-          listItemCollapseProps={{
-            disablePadding: true,
-            defaultOpen: true,
-            divider: false,
-            icon: <MoneyIcon />,
-            avatarVariant: 'circular'
-          }}
-        />
-      </Grid>
-    </EntriesProvider>
+      </EntriesProvider>
+    </>
   );
 }
