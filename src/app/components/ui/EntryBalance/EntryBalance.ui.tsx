b83b8ee21bc05c03b3a4abe0acbaafa655d37ccb
matheus correa <mathec@live.com>
2025-12-07 15:21:35 -0300

    feat: implements custom window confirm compoents

--- src/app/components/ui/EntryBalance/EntryBalance.ui.tsx
+++ src/app/components/ui/EntryBalance/EntryBalance.ui.tsx
@@ -3,19 +3,21 @@
 import { boardCopyAction, boardCreateAction, boardDeleteAction } from '@/app/actions/entries/entries.actions';
 import { ListContainer, ListItemInput, ListItemRow } from '@/app/components/elements';
 import { useEntriesContext } from '@/app/providers/entries/EntriesProvider';
+import { usePromptWindow } from '@/app/providers/prompt/PromptProvider';
 import AddIcon from '@mui/icons-material/AddOutlined';
 import BalanceIcon from '@mui/icons-material/Balance';
 import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import ShareIcon from '@mui/icons-material/Share';
 import Chip from '@mui/material/Chip';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { useId, useState } from 'react';
+import { useId } from 'react';
 
 
 export default function EntryBalance(props: { accountUuid: string }) {
+	const { confirm, alert, loading } = usePromptWindow();
 	const { entries, balance, boards, board, setBoard, setEntriesBoard, addEntries, findEntries } = useEntriesContext();
-	const [loading, setLoading] = useState(false);
 	const id = useId();
 
 	const handleBoardNameSubmit = async (value: string) => {
@@ -33,30 +35,38 @@ export default function EntryBalance(props: { accountUuid: string }) {
 				name: res.data.name,
 			});
 		} else {
-			alert(res.message || 'Erro ao nomear o painel');
+			await alert(res.message || 'Erro ao nomear o painel');
 		}
 	};
 
 	const handleDeleteBoard = async (boardUUid: string) => {
-		setLoading(true);
-		const entriesIds = findEntries(e => e.board?.uuid === boardUUid).map(e => e.id);
-		if (window.confirm([
+		const list = findEntries(e => e.board?.uuid === boardUUid);
+		const confirmation = await confirm(
 			'Atenção! Esta ação não pode ser desfeita.',
-			'Tem certeza que deseja deletar este painel?',
-			`${entriesIds.length} entradas vinculadas a este painel serão deletadas.`
-		].join('\n'))) {
-			const res = await boardDeleteAction({ uuid: boardUUid });
-			if (res.success) {
-				setEntriesBoard(entriesIds);
-			} else {
-				alert(res.message || 'Erro ao deletar o painel');
-			}
+			`Tem certeza que deseja deletar o painel ${list[0].board?.name} com ${list.length} entradas vinculadas? essas entradas serão deletadas.`,
+		);
+		if (!confirmation) {
+			return;
 		}
-		setLoading(false);
+		loading(true, 'Deletando painel...');
+		const res = await boardDeleteAction({ uuid: boardUUid });
+		if (res.success) {
+			setEntriesBoard(list.map(e => e.id));
+		} else {
+			await alert(res.message || 'Erro ao deletar o painel');
+		}
+		loading(false);
 	};
 
-	const cloneEmptyBoard = async () => {
-		setLoading(true);
+	const cloneBoard = async () => {
+		const confirmation = await confirm(
+			'Clonar painel',
+			`Deseja clonar o painel '${board!.name || ''}' com todas as suas entradas?`,
+		);
+		if (!confirmation) {
+			return;
+		}
+		loading(true, `Clonando painel '${board!.name || '...'}'`);
 		const res = await boardCopyAction({
 			accountUuid: props!.accountUuid,
 			boardId: board!.id,
@@ -67,21 +77,28 @@ export default function EntryBalance(props: { accountUuid: string }) {
 		} else {
 			alert(res.message || 'Erro ao clonar o painel');
 		}
-		setLoading(false);
+		loading(false);
 	};
+
+	const handleShareBoard = () => {
+		alert('Em breve você poderá compartilhar seus painéis com outras pessoas!');
+	};
+
 	return (
 		<>
 			{boards.length > 0 &&
 				<Stack direction='row' spacing={1} alignItems='center' mb={1} overflow='auto' py={1}>
-					<IconButton onClick={() => cloneEmptyBoard()} color='primary' disabled={!board?.id} loading={loading}>
+					<IconButton onClick={() => handleShareBoard()} color='primary' disabled={!board?.id}>
+						<ShareIcon />
+					</IconButton>
+					<IconButton onClick={() => cloneBoard()} color='primary' disabled={!board?.id}>
 						<AddIcon />
 					</IconButton>
 					{boards.map((b, i) => (
 						<Chip
 							key={id + b.name + i}
 							label={<Typography variant='subtitle2'>{b.name || '_______'}</Typography>}
-							disabled={loading}
-							deleteIcon={<DeleteIcon sx={{ ml: 1 }} />}
+							deleteIcon={<DeleteIcon sx={{ mx: 1 }} />}
 							onDelete={() => handleDeleteBoard(b.uuid)}
 							onClick={() => setBoard(b)}
 							color='primary'
---
+++ src/app/providers/prompt/PromptProvider.tsx
@@ -0,0 +1,154 @@
+'use client';
+
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { type TransitionProps } from '@mui/material/transitions';
+import Typography from '@mui/material/Typography';
+import { createContext, forwardRef, useContext, useRef, useState } from 'react';
+
+const Transition = forwardRef(function Transition(
+	props: TransitionProps & {
+		children: React.ReactElement;
+	},
+	ref: React.Ref<unknown>,
+) {
+	return <Slide direction="down" ref={ref} {...props} />;
+});
+
+
+interface IPromptContext {
+	confirm: (msg: string, caption: string) => Promise<boolean>;
+	alert: (msg: string) => Promise<boolean>;
+	loading: (state: boolean, msg?: string) => Promise<unknown>;
+}
+
+const PromptContext = createContext<IPromptContext | null>(null);
+
+enum PromptType {
+	CONFIRM = 'CONFIRM',
+	PROMPT = 'PROMPT',
+	ALERT = 'ALERT',
+	LOADING = 'LOADING',
+}
+
+interface IAction {
+	label: string;
+	action: () => void;
+	color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+}
+
+export function PromptProvider(props: { children: React.ReactNode }) {
+	const [open, setOpen] = useState(false);
+	const [title, setTitle] = useState<string | undefined>(undefined);
+	const [caption, setCaption] = useState<string | undefined>(undefined);
+	const [type, setType] = useState<`${PromptType}`>();
+	const [actions, setActions] = useState<IAction[]>([]);
+	const resolveCallbackRef = useRef<(value: boolean) => void>(() => new Promise((res) => res));
+
+	const prompt = (promptType: PromptType, title?: string, caption?: string, state?: boolean) => {
+		setOpen(true);
+		setType(promptType);
+		setTitle(title);
+		setCaption(caption);
+		switch (promptType) {
+			case PromptType.CONFIRM:
+				setActions([
+					{ label: 'Cancelar', action: () => handlePrompt(false), color: 'error' },
+					{ label: 'OK', action: () => handlePrompt(true), color: 'primary' },
+				]);
+				break;
+			case PromptType.ALERT:
+				setActions([{ label: 'OK', action: () => handlePrompt(true), color: 'primary' }]);
+				break;
+			case PromptType.LOADING:
+				setActions([]);
+				if (state === false) {
+					handlePrompt(false);
+				}
+				break;
+		}
+		return new Promise((res) => resolveCallbackRef.current = res) as Promise<boolean>;
+	};
+
+	const resetPrompt = () => {
+		setOpen(false);
+		setTitle(undefined);
+		setCaption(undefined);
+		setActions([]);
+		setType(undefined);
+	};
+
+	const handlePrompt = async (state: boolean) => {
+		if (type === PromptType.LOADING) {
+			// Prevent closing loading dialog by user
+			return;
+		}
+		resolveCallbackRef.current(state);
+		resetPrompt();
+	};
+
+	return (
+		<PromptContext.Provider value={{
+			confirm: (msg: string, caption: string) => prompt(PromptType.CONFIRM, msg, caption),
+			alert: (caption: string) => prompt(PromptType.ALERT, undefined, caption),
+			loading: (state: boolean, msg?: string) => prompt(PromptType.LOADING, msg || 'Carregando...', undefined, state),
+		}}>
+			{props.children}
+			<Dialog
+				open={open}
+				onClose={() => handlePrompt(false)} maxWidth="sm" fullWidth tabIndex={-1}
+				slots={{
+					transition: Transition,
+				}}
+				sx={{
+					'& .MuiDialog-container': {
+						alignItems: 'flex-start',
+					},
+				}}
+			>
+				{type === PromptType.LOADING &&
+					<DialogContent sx={{ display: 'flex', alignItems: 'center', gap: 3, justifyContent: 'flex-start', padding: 4 }}>
+						<CircularProgress size={24} />
+						<Typography variant="subtitle1" component="h3">
+							{title}
+						</Typography>
+					</DialogContent>
+				}
+
+				{type !== PromptType.LOADING && <>
+					{title &&
+						<DialogTitle>
+							{title}
+						</DialogTitle>
+					}
+					<DialogContent dividers hidden={!caption}>
+						<Typography variant="body1" component="h4">{caption}</Typography>
+					</DialogContent>
+					<DialogActions>
+						{actions.map((action, index) => (
+							<Button
+								key={`prompt-action-${index}`}
+								onClick={() => action.action()}
+								color={action.color}>
+								{action.label}
+							</Button>
+						))}
+					</DialogActions>
+				</>}
+			</Dialog>
+		</PromptContext.Provider>
+	);
+}
+
+export function usePromptWindow() {
+	const context = useContext(PromptContext);
+	if (!context) {
+		throw new Error('usePromptWindow must be used within a PromptProvider');
+	}
+	return context;
+}
