0fc7394caa9317138ca14713a6f4138509e6bdc8
matheus correa <mathec@live.com>
2026-01-02 12:43:40 -0300

    feat: improves code with topggle lock button

--- src/app/components/composites/EntryBalance/EntryBalance.ui.tsx
+++ src/app/components/composites/EntryBalance/EntryBalance.ui.tsx
@@ -1,105 +1,51 @@
 'use client';
 
-import { boardCopyAction, boardCreateAction, boardDeleteAction } from '@/app/actions/entries/entries.actions';
 import { ListContainer, ListItemInput, ListItemRow } from '@/app/components/elements';
+import useLocalStorage, { STORAGE } from '@/app/hooks/useLocalStorage.hook';
+import { useEntriesActions } from '@/app/providers/entries/EntriesActions';
 import { useEntriesContext } from '@/app/providers/entries/EntriesProvider';
-import { usePromptWindow } from '@/app/providers/prompt/PromptProvider';
 import AddIcon from '@mui/icons-material/AddOutlined';
 import BalanceIcon from '@mui/icons-material/Balance';
 import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import UnlockIcon from '@mui/icons-material/LockOpen';
+import LockIcon from '@mui/icons-material/LockOutline';
 import ShareIcon from '@mui/icons-material/Share';
 import Chip from '@mui/material/Chip';
+import Grow from '@mui/material/Grow';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { useId } from 'react';
 
-
 export default function EntryBalance(props: { accountUuid: string }) {
-	const { confirm, alert, loading } = usePromptWindow();
-	const { entries, balance, boards, board, setBoard, setEntriesBoard, addEntries, findEntries } = useEntriesContext();
+	const [lock, setLock] = useLocalStorage(STORAGE.LOCK_BOARD, true);
+	const { entries, balance, boards, board, setBoard } = useEntriesContext();
+	const { handleBoardNameSubmit, handleCloneBoard, handleDeleteBoard, handleShareBoard } = useEntriesActions(props.accountUuid);
 	const id = useId();
-
-	const handleBoardNameSubmit = async (value: string) => {
-		const res = await boardCreateAction({
-			accountUuid: props.accountUuid,
-			boardId: board?.id || 0,
-			boardName: value,
-			entriesIds: entries.map(e => e.id),
-		});
-		if (res.success) {
-			const entriesIds = entries.map(e => e.id);
-			setEntriesBoard(entriesIds, {
-				id: res.data.id,
-				uuid: res.data.uuid,
-				name: res.data.name,
-			});
-		} else {
-			await alert(res.message || 'Erro ao nomear o painel');
-		}
-	};
-
-	const handleDeleteBoard = async (boardUUid: string) => {
-		const list = findEntries(e => e.board?.uuid === boardUUid);
-		const confirmation = await confirm(
-			'Atenção! Esta ação não pode ser desfeita.',
-			`Tem certeza que deseja deletar o painel ${list[0].board?.name} com ${list.length} entradas vinculadas? essas entradas serão deletadas.`,
-		);
-		if (!confirmation) {
-			return;
-		}
-		loading(true, 'Deletando painel...');
-		const res = await boardDeleteAction({ uuid: boardUUid });
-		if (res.success) {
-			setEntriesBoard(list.map(e => e.id));
-		} else {
-			await alert(res.message || 'Erro ao deletar o painel');
-		}
-		loading(false);
-	};
-
-	const cloneBoard = async () => {
-		const confirmation = await confirm(
-			'Clonar painel',
-			`Deseja clonar o painel '${board!.name || ''}' com todas as suas entradas?`,
-		);
-		if (!confirmation) {
-			return;
-		}
-		loading(true, `Clonando painel '${board!.name || '...'}'`);
-		const res = await boardCopyAction({
-			accountUuid: props!.accountUuid,
-			boardId: board!.id,
-		});
-		if (res.success) {
-			addEntries(res.data);
-			setBoard(res.data[0].board!);
-		} else {
-			alert(res.message || 'Erro ao clonar o painel');
-		}
-		loading(false);
-	};
-
-	const handleShareBoard = () => {
-		alert('Em breve você poderá compartilhar seus painéis com outras pessoas!');
-	};
 
 	return (
 		<>
 			{boards.length > 0 &&
 				<Stack direction='row' spacing={1} alignItems='center' mb={1} overflow='auto' py={1}>
-					<IconButton onClick={() => handleShareBoard()} color='primary' disabled={!board?.id}>
-						<ShareIcon />
+					<IconButton onClick={() => setLock(!lock)} color={lock ? 'primary' : 'secondary'}>
+						{lock ? <LockIcon /> : <UnlockIcon />}
 					</IconButton>
-					<IconButton onClick={() => cloneBoard()} color='primary' disabled={!board?.id}>
-						<AddIcon />
-					</IconButton>
+					<Grow in={!lock}>
+						<div style={{ width: !lock ? 'auto' : 0, display: 'flex' }}>
+							<IconButton onClick={() => handleShareBoard()} color='primary' disabled={!board?.id}>
+								<ShareIcon />
+							</IconButton>
+							<IconButton onClick={() => handleCloneBoard()} color='primary' disabled={!board?.id}>
+								<AddIcon />
+							</IconButton>
+						</div>
+					</Grow>
 					{boards.map((b, i) => (
 						<Chip
 							key={id + b.name + i}
 							label={<Typography variant='subtitle2'>{b.name || '_______'}</Typography>}
 							deleteIcon={<DeleteIcon sx={{ mx: 1 }} />}
-							onDelete={() => handleDeleteBoard(b.uuid)}
+							onDelete={!lock ? () => handleDeleteBoard(b.uuid) : undefined}
 							onClick={() => setBoard(b)}
 							color='primary'
 							variant={board?.uuid === b.uuid ? 'filled' : 'outlined'}
